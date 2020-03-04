8ec5bb074eb3207acc38a5a7af444b8a84b59c05
zhangchi56 <591481308@qq.com>
2020-03-04 23:42:40 +0800

    加油

--- src/router.js
+++ src/router.js
@@ -5,31 +5,37 @@ import routes from "./common/config/router.js"
 
 Vue.use(Router)
 
-let router = new Router({ routes })
+let router = new Router({
+  routes
+})
 
 // 全局前置守卫
 router.beforeEach((to, from, next) => {
-	// 获取token
-	let token = window.sessionStorage.getItem('token')
-	
-	if(token){ // 已登录
-		// 防止重复登录
-		if(to.path === '/login'){
-			Vue.prototype.$message.error('请不要重复登录');
-			return next({name:from.name ? from.name : 'index'})
-		}
-		// 其他验证...
-		
-		next();
-	} else {
-		// 跳过登录页验证
-		if(to.path === '/login'){
-			return next();
-		}
-		// 未登录
-		Vue.prototype.$message.error('请先登录');
-		next({ path:'/login' })
-	}
+  // 获取token
+  let token = window.sessionStorage.getItem('token')
+
+  if (token) { // 已登录
+    // 防止重复登录
+    if (to.path === '/login') {
+      Vue.prototype.$message.error('请不要重复登录');
+      return next({
+        name: from.name ? from.name : 'index'
+      })
+    }
+    // 其他验证...
+
+    next();
+  } else {
+    // 跳过登录页验证
+    if (to.path === '/login') {
+      return next();
+    }
+    // 未登录
+    Vue.prototype.$message.error('请先登录');
+    next({
+      path: '/login'
+    })
+  }
 })
 
 export default router
